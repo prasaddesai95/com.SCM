f6a681615f3d6e67fa8b3edf5f7ad3c6fa7b9e23
Dell <Dell@DESKTOP-NQP4P5N>
2023-11-28 12:14:03 +0530

    updated test scripts updated

---
+++ src/test/java/com/SCM/Procure/TestScript1Test.java
@@ -0,0 +1,22 @@
+package com.SCM.Procure;
+
+import org.testng.annotations.Test;
+
+public class TestScript1Test {
+	@Test(groups={"smoke"})
+public void ts1() {
+	System.out.println("==TS1===");
+}
+	@Test(groups={"smoke","regression"})
+public void ts2() {
+	System.out.println("==TS2===");
+}
+	@Test(groups={"smoke"})
+public void ts3() {
+	System.out.println("=TS 3===");
+}
+	@Test(groups={"regression"})
+	public void ts4() {
+		System.out.println("==TS 4===");
+	}
+}
